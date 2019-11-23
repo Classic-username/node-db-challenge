568533b8854bd3b786f2e0094ac26eeace289740
Joshua Draper <josh.drapergame@gmail.com>
2019-11-22 20:01:59 -0700

    migrations written

---
+++ data/migrations/20191122192948_workv1.js
@@ -0,0 +1,52 @@
+
+exports.up = function(knex) {
+    return (
+        knex.schema
+            .createTable('project', tbl => {
+                tbl.increments()
+                tbl.text('name').notNullable()
+                tbl.text('description')
+                tbl.boolean(false)
+            })
+            .createTable('resource', tbl => {
+                tbl.increments()
+                tbl.text('name').unique().notNullable()
+                tbl.text('description')
+            })
+            .createTable('task', tbl => {
+                tbl.increments()
+                tbl.text('description').notNullable()
+                tbl.text('info')
+                tbl.boolean(false)
+                tbl.integer('project_id')
+                    .unsigned()
+                    .notNullable()
+                    .references('project.id')
+                    .onDelete('CASCADE')
+                    .onUpdate('CASCADE')
+            })
+            .createTable('project_resource', tbl => {
+                tbl.increments()
+                tbl.integer('project_id')
+                    .unsigned()
+                    .notNullable()
+                    .references('project.id')
+                    .onDelete('CASCADE')
+                    .onUpdate('CASCADE')
+                tbl.integer('resource_id')
+                    .unsigned()
+                    .notNullable()
+                    .references('resource.id')
+                    .onDelete('CASCADE')
+                    .onUpdate('CASCADE')
+            })
+    )
+
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('project_resource')
+    return knex.schema.dropTableIfExists('task')
+    return knex.schema.dropTableIfExists('resource')
+    return knex.schema.dropTableIfExists('project')
+};
